b449431d26bc842d15c51504802b9e2af69283c8
hata05002 <112382754+hata05002@users.noreply.github.com>
2022-11-07 09:34:29 +0100

    Add files via upload
    nummer 2 är första uppgiften

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+class program{
+      public static void Main()
+{
+	Console.WriteLine("10 upphöjt till 7: "+Power(10,7));
+	Console.WriteLine("2 upphöjt till 10: "+Power(2,10));
+}
+
+static double Power(double a, int b){
+double tal=1;
+for(int i=0; i<b;i++) {
+    tal= tal*a;
+}
+return tal;
+}
+
+}
